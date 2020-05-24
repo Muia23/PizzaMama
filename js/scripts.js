f00936f4ccfc1c1127da38c9acafab7712a34080
Muia23 <wilfred.muema@gmail.com>
2020-05-24 15:18:56 +0300

    add function to total customer's choice

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Carousel
 var slideIndex = 0;
 showSlides();
 
@@ -11,4 +12,15 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+var totalSum = 0
+window.onload=function sumtopping(){
+const addchoice = document.getElementsByClassName('choice');
+const totalchoice  = document.getElementById('choiceTotal');
+ for (var i=0; i < addchoice.length; i++) {
+    addchoice[i].onchange = function() {
+        var add = this.value * (this.checked ? 1 : -1);
+    return totalSum = totalchoice.innerHTML = parseFloat(totalchoice.innerHTML) + add;
+    }
+  }
 }
